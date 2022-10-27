1f66153f2d2fd09b3c82ed8c88808aaceac25e34
Iurii Tankievych <iurii.tankievych@outlook.com>
2022-10-27 15:50:25 +0100

    fix

--- src/pages/exercise1.tsx
+++ src/pages/exercise1.tsx
@@ -5,17 +5,18 @@ export const Exercise1 = () => {
   const { isWalletConnected, enableNextStep } = useContext(AppContext)
 
   useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'http://localhost:8080/widget-poap.js'
+    script.defer = true
+    document.body.appendChild(script)
+  }, [])
+
+  useEffect(() => {
     enableNextStep(isWalletConnected)
   }, [isWalletConnected])
 
   return (
     <>
-      {/* https://github.com/nevermined-io/nvm-one-widgets */}
-      <script
-        defer
-        type="text/javascript"
-        src="/lib/widget-poap.js"
-      ></script>
       <div
         id="poap-widget"
         nvm-did="did:nv:2c66b9b65c71951b0c5155bfe6c374d68cc7483d715b11e27a39f286ca3d0722"
