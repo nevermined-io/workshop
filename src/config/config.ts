62a10800ccecbe07bfb029251b46ad4930af902d
spieljs <spieljs@gmail.com>
2022-10-28 18:58:41 +0200

    add style to widget

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,21 +8,23 @@ export const faucetUri = 'https://faucet.rinkeby.nevermined.rocks'
 export const acceptedChainId = '80001' // for Mumbai
 export const rootUri = 'http://localhost:3445'
 export const marketplaceUri = 'https://marketplace-api.mumbai.public.nevermined.network'
-const graphHttpUri = 'https://api.thegraph.com/subgraphs/name/nevermined-io/common'
+const graphHttpUri = 'https://api.thegraph.com/subgraphs/name/nevermined-io/public'
 export const erc20TokenAddress = '0xe11a86849d99f524cac3e7a0ec1241828e332c62'
 export const nodeUri = 'https://matic-mumbai.chainstacklabs.com'
 export const filecoinUploadUri = '/api/v1/gateway/services/upload/filecoin'
 export const nftAddress = '0xAE04cD6F0d4026238AF24729640d6952a8649eea'
+export const poapDIDZeroX = '0xc6ad11d31da9baeebf2b827a0bbe6c3883c5de2b764d0fc48ed4ef5dfa249ba6'
 
 export const appConfig: Config = {
   web3Provider:
     typeof window !== 'undefined' ? window.ethereum : new ethers.providers.JsonRpcProvider(nodeUri),
   gatewayUri,
   faucetUri,
-  verbose: true,
+  verbose: 2,
   gatewayAddress,
   graphHttpUri,
   marketplaceAuthToken: AuthToken.fetchMarketplaceApiTokenFromLocalStorage().token,
   marketplaceUri,
   artifactsFolder: `${rootUri}/contracts`,
+  newGateway: true,
 }
